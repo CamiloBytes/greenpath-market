6aace7a87a720bec07555b95c254e2c6f15dcfee
unknown <camiloandres02222@gmail.com>
2026-08-07 22:29:39 -0500

    feat: add dashboard layout and page with banner carousel
    - Implemented a new DashboardLayout component for the dashboard page.
    - Created a DashboardPage component that includes a welcome message and a BannerCarousel.
    - Added BannerCarousel component with animated transitions and floating icons.
    - Introduced new banner data structure for carousel images and descriptions.
    - Added background images for the dashboard and carousel slides.
    - Updated layout and styling for improved user experience.
    - Removed unused AuthForm component and replaced it with a new AuthPage component for login and registration.
    - Refactored Input and Button components for better usability and styling.
    - Added Navbar component with search functionality and navigation links.

--- src/components/form/LoginForm/LoginForm.tsx
+++ src/components/form/LoginForm/LoginForm.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Input } from '../ui/Input';
-import { Button } from '../ui/Button';
+import { Input } from '../../ui/Input';
+import { Button } from '../../ui/Button';
 
 export const LoginForm = () => {
   return (
     <form 
-        className=" flex flex-col items-center jus gap-6 md:gap-8 w-8/10 max-w-md"
+        className=" flex flex-col items-center justify-center gap-6 md:gap-8 w-8/12"
     >
         <div className="w-full flex flex-col gap-2 justify-start items-start text-white">
         <h1
@@ -19,15 +19,13 @@ export const LoginForm = () => {
             type="email"
             placeholder="Email"
             label="Email"
-            id="email"
         />
         <Input 
             type="password"
             placeholder="Password"
             label="Password"
-            id="password"
         />
-        <Button className="w-1/2 transform hover:scale-105 transition duration-300" >
+        <Button type="submit" className="mt-10 w-1/2 transform hover:scale-105 transition duration-300" >
             sign in
         </Button>
     </form>
